cc42a76efd902268bb361511c6b532b23337a8e4
SanthoshTR-coder <santhoshtr0912@gmail.com>
2025-07-14 00:10:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ connectDB(); // ✅ Now it connects to MongoDB
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ['https://authfd.vercel.app', 'https://authfd-u6ii.vercel.app'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // ✅ Use the auth routes at /api/auth/*
